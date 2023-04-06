377fad2843665ef806632825d12a8469349f9ae6
Felicia Ayodeji <felicia.ayodeji55@gmail.com>
2023-04-06 10:10:13 +0200

    touchable

--- jour3-composants/composant/Touchable.jsx
+++ jour3-composants/composant/Touchable.jsx
@@ -4,21 +4,47 @@ import {
 	StyleSheet,
 	 Text, 
 	 View ,
+	 Image,
 	 TouchableHighlight,
 	 TouchableNativeFeedback, // ne fonctionne que sur les téléphone andriod
 	 TouchableOpacity,
 	 TouchableWithoutFeedback
+	 // site internet => https://icons.getbootstrap.com/icons/android2/
+	 // télécharger une image de votre choix 
+	 //stocker dans le doisser assets du projet jour3-composants
 	} from 'react-native'
 import React from 'react'
 
 const Touchable = () => {
   return (
-	<View>
+	<View style={styles.box}>
 	  <Text>Touchable</Text>
+	  <View style={{flexDirection :"row",justifyContent:"space-evenly"}}>
+	  <TouchableOpacity style={styles.touchableOpacity} >
+		<Image source ={{uri :"https://via.placeholder.com/50x50" , width :50 , height :50}} />
+	  </TouchableOpacity>
+	  {/** couleur actuelle => */}
+	  <TouchableNativeFeedback style={ {padding:20}} background={TouchableNativeFeedback.Ripple(
+       "#000",
+	   "#fff",
+	  )} onPress={()=> {}}>
+	  <Image source ={{uri :"https://via.placeholder.com/50x50" , width :50 , height :50}} />
+	  </TouchableNativeFeedback>
+	  <TouchableHighlight style={styles.touchableOpacity} activeOpacity={0.6} underlayColor="#DDDDDD" onPress={()=> {}}>
+	  <Image source ={{uri :"https://via.placeholder.com/50x50" , width :50 , height :50}} />
+	  </TouchableHighlight>
+	  <TouchableWithoutFeedback onPress={() => {console.warn("no feed back")}} style={styles.touchableOpacity}>
+	  <Image source ={{uri :"https://via.placeholder.com/50x50" , width :50 , height :50}} />
+	  </TouchableWithoutFeedback>
+	  </View>
+	  
 	</View>
   )
-}
+} 
 
 export default Touchable
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	box : {paddingHorizontal :10},
+	touchableOpacity :{width :100, height :100 ,backgroundColor :"#eee", justifyContent : "center", alignItems : "center" }
+})

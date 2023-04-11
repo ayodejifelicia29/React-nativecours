24395e58880654af2c643ffe56517ba6cb27a06c
Felicia Ayodeji <felicia.ayodeji55@gmail.com>
2023-04-11 14:02:21 +0200

    ajoute des image

--- jour6-menu/App.js
+++ jour6-menu/App.js
@@ -1,10 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
+
+
+import  Accueil from "./composants/Accueil"
+import Profil from './composants/Profil';
+
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const Menu =createBottomTabNavigator()
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <NavigationContainer>
+        <Menu.Navigator screenOptions={{
+          tabBarActiveBackgroundColor :"#eee",
+          tabBarStyle :{borderColor :"red",borderWidth:3},
+          tabBarShowLabel:false,
+          title :"bonjour"
+        }} >
+            {/**https://reactnavigation.org/docs/buttom-tab-navigator */}
+        <Menu.Screen name='home'component ={Accueil} 
+              options={{
+                tabBarIcon :function () {
+                  return <MaterialCommunityIcons name="air-horn" color="black" size={40}/>
+                }
+              }}  />
+              
+            {/**npm i ... => https://www.npmjs.com/ 
+           *  react-native-vector-icons
+           * https://www.npmjs.com/package/react-native-vector-icons 
+           * https://pictogrammers.com/library/mdi/*/}
+        <Menu.Screen name='profil'component ={Profil} 
+            options={{
+              tabBarIcon :function () {
+                return <MaterialCommunityIcons name="anchor" color="black" size={40}/>
+              }
+            }}
+          />
+       
+          </Menu.Navigator>
+           </NavigationContainer>
+     
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +53,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
 });

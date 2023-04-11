e14a2be7bc04934e3a168f23b5159c965972f1d9
Felicia Ayodeji <felicia.ayodeji55@gmail.com>
2023-04-11 15:43:31 +0200

    lien navigation

--- jour6-menu/App.js
+++ jour6-menu/App.js
@@ -1,49 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {NavigationContainer} from "@react-navigation/native";
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
 
+import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import  Accueil from "./composants/Accueil"
-import Profil from './composants/Profil';
+import Accueil from "./composants/Accueil"
+import ProfilNavigation from "./composants/ProfilNavigation"
 
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const Menu =createBottomTabNavigator()
+const Menu = createBottomTabNavigator()
 
 export default function App() {
   return (
     <View style={styles.container}>
       <NavigationContainer>
-        <Menu.Navigator screenOptions={{
-          tabBarActiveBackgroundColor :"#eee",
-          tabBarStyle :{borderColor :"red",borderWidth:3},
-          tabBarShowLabel:false,
-          title :"bonjour"
-        }} >
-            {/**https://reactnavigation.org/docs/buttom-tab-navigator */}
-        <Menu.Screen name='home'component ={Accueil} 
-              options={{
-                tabBarIcon :function () {
-                  return <MaterialCommunityIcons name="air-horn" color="black" size={40}/>
-                }
-              }}  />
-              
-            {/**npm i ... => https://www.npmjs.com/ 
+      <Menu.Navigator screenOptions={{
+        tabBarActiveBackgroundColor : "#eee",
+        tabBarStyle : { borderColor : "red" , borderWidth : 3 },
+        tabBarShowLabel : false, 
+       /*  title : ((text) => { return <View>
+          <Text>{JSON.stringify(text)}</Text>
+          <MaterialCommunityIcons name="air-horn" color="black" size={40} />
+          </View>  
+        })() */
+      }}>
+        {/** https://reactnavigation.org/docs/bottom-tab-navigator */}
+        <Menu.Screen name="home" component={Accueil} 
+          options={{
+            tabBarIcon : function(){
+              return <MaterialCommunityIcons name="home-heart" color="black" size={40} />
+            }
+          }} />
+          {/**  npm i ... => https://www.npmjs.com/ 
            *  react-native-vector-icons
-           * https://www.npmjs.com/package/react-native-vector-icons 
-           * https://pictogrammers.com/library/mdi/*/}
-        <Menu.Screen name='profil'component ={Profil} 
-            options={{
-              tabBarIcon :function () {
-                return <MaterialCommunityIcons name="anchor" color="black" size={40}/>
-              }
-            }}
-          />
-       
-          </Menu.Navigator>
-           </NavigationContainer>
-     
+           * https://www.npmjs.com/package/react-native-vector-icons
+           * https://pictogrammers.com/library/mdi/
+           */}
+        <Menu.Screen name="profil" component={ ProfilNavigation } options={{
+          tabBarIcon : function(){
+            return <MaterialCommunityIcons name="account-box" color="black" size={40} />
+          }
+        }}/>
+         
+       </Menu.Navigator>
+
+      </NavigationContainer>
       <StatusBar style="auto" />
     </View>
   );
@@ -52,7 +54,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    
+    backgroundColor: '#fff'
   },
 });
--- jour6-menu/composants/Profil.jsx
+++ jour6-menu/composants/Profil.jsx
@@ -1,26 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View , TouchableWithoutFeedback , Button } from 'react-native'
 import React from 'react'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const Profil = () => {
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+
+const Profil = ({navigation}) => {
   return (
-	<View style={styles.box}>
-	  <Text style={styles.titre}>Profil</Text>
-	</View>
+    <View style={styles.box}>
+      <Text  style={styles.titre}>Profil</Text>
+      {/** https://pictogrammers.com/library/mdi/ */}
+      <TouchableWithoutFeedback onPress={() => alert("coucou")}>
+        <MaterialCommunityIcons size={50} color="red" name="account"/>
+      </TouchableWithoutFeedback>
+      <Button onPress={() => { navigation.navigate("modifier-login") }} title="modifier mot de passe" />
+    </View>
   )
 }
 
 export default Profil
 
 const styles = StyleSheet.create({
-	box:{
-		flex: 1,
-		justifyContent:"center",
-		alignItems:"center"
-	},
-	titre:{
-		fontSize: 30,
-	}
+    box : {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    titre : {
+        fontSize : 30,
+    }
 })
-
-//export
-// exp

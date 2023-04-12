8d15695e95ed9ad70d6898eef4128a6927b0f6e7
Felicia Ayodeji <felicia.ayodeji55@gmail.com>
2023-04-12 10:36:07 +0200

    etape 1 context

---
+++ jour7-context/contexts/profilContext.js
@@ -0,0 +1,11 @@
+// vous pouvez l'appel comme vous volez
+// dans ce fichier que l'on créer un state global
+
+import{createContext} from "react" // cette fonction qui permet de créer un état global
+
+export const ProfilConxtext = createContext(); // store
+
+export function ProfilConxtextProvider (){
+	// un composabt qui va emballer l'intégralité de notre application
+
+}

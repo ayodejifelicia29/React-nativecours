1f761a82dcabea1d35e77eb0910a14fbfb9d5d8b
Felicia Ayodeji <felicia.ayodeji55@gmail.com>
2023-04-12 17:26:11 +0200

    fin

--- jour7-context/App.js
+++ jour7-context/App.js
@@ -1,27 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+  import { StatusBar } from 'expo-status-bar';
+  import { StyleSheet, View } from 'react-native';
 
-import { ProfilContextProvider } from './contexts/profilContext';
-import Menu from './composant/Menu';
-import { SelectionContextProvider } from './contexts/selectionContext';
+  import { ProfilContextProvider } from './contexts/profilContext';
+  import Menu from './composant/Menu';
+  import { SelectionContextProvider } from './contexts/selectionContext';
 
-export default function App() {
+  export default function App() {
 
-  return (
-    <View style={styles.container}>
-      <ProfilContextProvider>
-        <SelectionContextProvider>
-        <Menu />
-        </SelectionContextProvider>
-      </ProfilContextProvider>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    return (
+      <View style={styles.container}>
+        <ProfilContextProvider>
+          <SelectionContextProvider>
+          <Menu />
+          </SelectionContextProvider>
+        </ProfilContextProvider>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff'
+    },
+  });
--- jour7-context/composant/Formulaire.jsx
+++ jour7-context/composant/Formulaire.jsx
@@ -1,57 +1,57 @@
-import { StyleSheet, Text, View , TextInput, Button } from 'react-native'
-import React , {useContext , useState} from 'react'
-import { ProfilContext } from "../contexts/profilContext"
+		import { StyleSheet, Text, View , TextInput, Button } from 'react-native'
+		import React , {useContext , useState} from 'react'
+		import { ProfilContext } from "../contexts/profilContext"
 
-const Formulaire = () => {
+		const Formulaire = () => {
 
-  const { profil, login , logout } = useContext(ProfilContext);
-  const [email , setEmail] = useState("")
-  const [password , setPassword] = useState("")
-  const [message , setMessage] = useState("")
-   function authentification(){
-    const identifiants = {
-      email : email ,
-      password : password
-    }
-    //console.log(identifiants)
-    const verif = login(identifiants)
-    //console.log(profil)
-    if(verif){ // si je n'ai pas de message = "" => je peux vider le formulaire
-      // vider le formulaire et le message
-      setEmail("")
-      setPassword("")
-      setMessage("")
-    }else {
-      // sinon j'affiche un message 
-      setMessage("identifiants invalides")
-    }
-  }
+		const { profil, login , logout } = useContext(ProfilContext);
+		const [email , setEmail] = useState("")
+		const [password , setPassword] = useState("")
+		const [message , setMessage] = useState("")
+		function authentification(){
+			const identifiants = {
+			email : email ,
+			password : password
+			}
+			//console.log(identifiants)
+			const verif = login(identifiants)
+			//console.log(profil)
+			if(verif){ // si je n'ai pas de message = "" => je peux vider le formulaire
+			// vider le formulaire et le message
+			setEmail("")
+			setPassword("")
+			setMessage("")
+			}else {
+			// sinon j'affiche un message 
+			setMessage("identifiants invalides")
+			}
+		}
 
-  return (
-    <View style={styles.box}>
-      { profil.isLogged  
-        ? 
-         <Button  onPress={() => logout()} title="deconnexion" />
-        : 
-          <View>
-            <Text style={styles.form}>Formulaire</Text>
-            <TextInput placeholder='email' style={styles.input} onChangeText={(text) => setEmail(text)} value={email}/>
-            <TextInput placeholder='password' style={styles.input} onChangeText={(text) => setPassword(text)} value={password} />
-            <Button title="soumettre" onPress={() => authentification()}/>
-            {message.length > 0  && <Text style={styles.alert}>{message}</Text>}
-          </View>
-      }
-      
-    </View>
-  )
-}
+		return (
+			<View style={styles.box}>
+			{ profil.isLogged  
+				? 
+				<Button  onPress={() => logout()} title="deconnexion" />
+				: 
+				<View>
+					<Text style={styles.form}>Formulaire</Text>
+					<TextInput placeholder='email' style={styles.input} onChangeText={(text) => setEmail(text)} value={email}/>
+					<TextInput placeholder='password' style={styles.input} onChangeText={(text) => setPassword(text)} value={password} />
+					<Button title="soumettre" onPress={() => authentification()}/>
+					{message.length > 0  && <Text style={styles.alert}>{message}</Text>}
+				</View>
+			}
+			
+			</View>
+		)
+	}
 
-export default Formulaire
+		export default Formulaire
 
-const styles = StyleSheet.create({
-    box : { margin : 10},
-    input : { borderWidth : 1 , borderColor : "black" , padding : 5 , marginBottom : 10},
-    alert : { padding : 10 , color : "red"},
-	form:{margin:10}
+		const styles = StyleSheet.create({
+			box : { margin : 10},
+			input : { borderWidth : 1 , borderColor : "black" , padding : 5 , marginBottom : 10},
+			alert : { padding : 10 , color : "red"},
+			form:{margin:10}
 
-})
+		})

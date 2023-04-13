159f98dd7e32aca84865aaea5215cf2def9f1a83
Felicia Ayodeji <felicia.ayodeji55@gmail.com>
2023-04-13 14:32:00 +0200

    useContext+context+useReducer

--- jour8-redux/App.js
+++ jour8-redux/App.js
@@ -3,15 +3,22 @@ import { StyleSheet, Text, View } from 'react-native';
 import A from './composant/A';
 import B from './composant/B';
 import Exo1 from './composant/Exo1';
+import { ExoContextProvider } from './contexts/exoContext';
+import Exo from './composant/Exo';
 
 export default function App() {
   return (
+    <ExoContextProvider>
     <View style={styles.container}>
-      <Exo1/>
-       <A/>
-       <B/>
+      <Exo/>
+      {false && <>
+        <Exo1 />
+        <A />
+        <B />
+      </>}
       <StatusBar style="auto" />
     </View>
+    </ExoContextProvider>
   );
 }
 
